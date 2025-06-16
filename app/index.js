5b8fe6bd2b2cbb49cfdf722657d3701b7c7319c0
konika-singh <konikasingh1996@gmail.com>
2025-06-16 23:39:57 +0530

    first commit111111

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World from Docker + Terraform + GitHub Actions!');
+  res.end('Hello World from Docker + Terraform + GitHub Actions! yesss its done done done hurrey');
 });
 
 server.listen(3000, () => {
