e945b3270ad3dbc8790ccd3a50f6e9957946fc79
konika-singh <konikasingh1996@gmail.com>
2025-06-16 22:50:05 +0530

    first commit1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Docker + Terraform + GitHub Actions CI/CD Project
+
+A sample project to demonstrate deploying a Node.js app to AWS EC2 using Docker, Terraform, and GitHub Actions.
---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "docker-key"
+  public_key = file("C:/Users/Konika/.ssh/docker-key.pub")
+}
+
+resource "aws_security_group" "allow_web_ssh" {
+  name        = "allow_web_ssh"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "docker_app" {
+  ami                    = "ami-03bb6d83c60fc5f7c"  # ✅ Ubuntu AMI
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_web_ssh.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y docker.io
+              systemctl start docker
+              docker run -d -p 80:3000 ksingh787/hello-app
+              EOF
+
+  tags = {
+    Name = "DockerAppServer"
+  }
+}
