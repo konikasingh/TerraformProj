8f14f4603344013ff9d9dd33341e21e7f91d7812
konika-singh <konikasingh1996@gmail.com>
2025-06-17 14:22:54 +0530

    Setup dev environment CI/CD1dev

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,3 +1,4 @@
+
 provider "aws" {
   region = "ap-south-1"
 }
@@ -5,7 +6,7 @@ provider "aws" {
 module "app_server" {
   source            = "../modules/ec2_docker_app"
   key_name          = "docker-key-prod"
-  public_key_file   = "../docker-key.pub"
+  public_key        = file("${path.root}/../docker-key.pub")  # ✅ Loads content
   sg_name           = "allow_web_ssh_prod"
   ami               = "ami-03bb6d83c60fc5f7c"
   instance_type     = "t2.micro"
--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,15 +1,14 @@
-
 provider "aws" {
   region = "ap-south-1"
 }
 
 module "app_server" {
   source            = "../modules/ec2_docker_app"
-  key_name          = "docker-key-qa"
+  key_name          = "docker-key-stage"
   public_key        = file("${path.root}/../docker-key.pub")  # ✅ Loads content
-  sg_name           = "allow_web_ssh_qa"
+  sg_name           = "allow_web_ssh_stage"
   ami               = "ami-03bb6d83c60fc5f7c"
   instance_type     = "t2.micro"
   docker_image      = "ksingh787/hello-app"
-  instance_name     = "QaAppServer"
+  instance_name     = "StageAppServer"
 }
