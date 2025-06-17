62dcec1b08ffeda93c8dbe7104df9183c9fc6e90
konika-singh <konikasingh1996@gmail.com>
2025-06-17 20:35:30 +0530

    addede to prod 1

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 
 module "app_server" {
   source            = "../modules/ec2_docker_app"
-  key_name          = "docker-key-prod"
+  key_name          = "docker-key-prod-i"
   public_key        = file("${path.root}/../docker-key.pub")  # ✅ Loads content
-  sg_name           = "allow_web_ssh_prod"
+  sg_name           = "allow_web_ssh_prod-i"
   ami               = "ami-03bb6d83c60fc5f7c"
   instance_type     = "t2.micro"
   docker_image      = "ksingh787/hello-app"
