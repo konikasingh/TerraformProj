1423f8112d9bf2be435ba547e9141e7b81ba3732
konika-singh <konikasingh1996@gmail.com>
2025-06-17 14:50:33 +0530

    Setup dev environment CI/CD1dev1

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -4,11 +4,11 @@ provider "aws" {
 
 module "app_server" {
   source            = "../modules/ec2_docker_app"
-  key_name          = "docker-key-dev"
+  key_name          = "docker-key-dev1"
   public_key        = file("${path.root}/../docker-key.pub")  # ✅ Loads content
-  sg_name           = "allow_web_ssh_dev"
+  sg_name           = "allow_web_ssh_dev11"
   ami               = "ami-03bb6d83c60fc5f7c"
   instance_type     = "t2.micro"
   docker_image      = "ksingh787/hello-app"
-  instance_name     = "DevAppServer"
+  instance_name     = "DevAppServer2"
 }
