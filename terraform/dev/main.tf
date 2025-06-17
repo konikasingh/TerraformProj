5128b51aa50be7132365e8bc3b2f301e3b1362b4
konika-singh <konikasingh1996@gmail.com>
2025-06-17 15:43:30 +0530

    addrd added commit

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -4,11 +4,11 @@ provider "aws" {
 
 module "app_server" {
   source            = "../modules/ec2_docker_app"
-  key_name          = "docker-key-dev1"
+  key_name          = "docker-key-dev123"
   public_key        = file("${path.root}/../docker-key.pub")  # ✅ Loads content
-  sg_name           = "allow_web_ssh_dev11"
+  sg_name           = "allow_web_ssh_dev112233"
   ami               = "ami-03bb6d83c60fc5f7c"
   instance_type     = "t2.micro"
   docker_image      = "ksingh787/hello-app"
-  instance_name     = "DevAppServer2"
+  instance_name     = "DevAppServer3"
 }
